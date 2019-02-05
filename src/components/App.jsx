63badacb57796822171c7f99dee6ccbdb4b90e42
SokoPaulSokool <sopapaso73@gmail.com>
2019-02-05 15:30:57 +0300

    ch(setup-redux) set up redux
    add store reducers and action folders
    
    [Finishes #163689472]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import './App.scss';
+import store from '../store';
 
-class App extends Component {
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return <div>Fast food fast</div>;
+    return (
+      <Provider store={store}>
+        <div>Fast food fast</div>
+      </Provider>
+    );
   }
 }
+
+App.propTypes = {};
 
 export default App;
