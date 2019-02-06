e2ec64ec001243ca36f0601ee2b84315d04b310c
SokoPaulSokool <sopapaso73@gmail.com>
2019-02-06 10:30:05 +0300

    ch(add-router): add routing to project
    [Finishes #163689506]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import './App.scss';
 import store from '../store';
-import Routes from '../RouterComponent';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RouterComponent from '../RouterComponent';
+
 export class App extends Component {
   constructor(props) {
     super(props);
